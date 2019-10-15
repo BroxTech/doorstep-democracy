085dcf7196c20eb1255296d3d7082a8fe4b80f1f
BroxTech <56609006+BroxTech@users.noreply.github.com>
2019-10-15 21:20:53 +0100

    First Ever Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BroxTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
